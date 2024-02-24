af532dcb93a2c8f1405e9f2e7ff770ae101bb6fc
rsbastos <renedossantosbastos@gmail.com>
2024-02-24 09:26:35 -0300

    :sparkles: feat: main functions signatures

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -19,6 +19,11 @@ const state = {
     },    
 };
 
+const playerSides = {
+    player1: "player-field-card",
+    computer: "computer-field-card",
+}
+
 const pathImages = ".src/assets/icons/";
 
 const cardData = [
@@ -48,8 +53,18 @@ const cardData = [
     },
 ];
 
-function init() {
+async function drawCards(cardNumbers, fieldSide) {
+    for(let i = 0; i < cardNumbers; i++) {
+        const randomIdCard = await getRandomCardId();
+        const cardImage = await creatCardImage(randomIdCard, fieldSide);
 
+        document.getElementById(fieldSide).appendChild(cardImage);
+    }
+}
+
+function init() {
+    drawCards(5, playerSides.player1);
+    drawCards(5, playerSides.computer);
 }
 
 init();
