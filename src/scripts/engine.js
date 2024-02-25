1c0fec418be85f8335c30f247b483bd3f4bd5851
rsbastos <renedossantosbastos@gmail.com>
2024-02-25 19:14:32 -0300

    :recycle: refactor: extract to method

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -90,16 +90,37 @@ async function setCardsField(cardId) {
 
     let computerCardId = await getRandomCardId();
 
-    state.fieldCards.player.style.display = "block";
-    state.fieldCards.computer.style.display = "block";
+    await showHiddenCardFieldsImages(true);
 
-    state.fieldCards.player.src = cardData[cardId].img; 
-    state.fieldCards.computer.src = cardData[computerCardId].img; 
+    await hiddenCardDetails();  
+    
+    await drawCardsInField(cardId, computerCardId);
 
     let duelResults = await checkDuelResults(cardId, computerCardId);
 
     await updateScore();
     await drawButton(duelResults);
+}
+
+async function drawCardsInField(cardId, computerCardId) {
+    state.fieldCards.player.src = cardData[cardId].img; 
+    state.fieldCards.computer.src = cardData[computerCardId].img; 
+}
+
+async function showHiddenCardFieldsImages(value) {
+    if(value === true) {
+        state.fieldCards.player.style.display = "block";
+        state.fieldCards.computer.style.display = "block";
+    } else {
+        state.fieldCards.player.style.display = "none";
+        state.fieldCards.computer.style.display = "none";
+    };
+};
+
+async function hiddenCardDetails() {
+    state.cardSprites.avatar.src = "";
+    state.cardSprites.name.innerText = "";
+    state.cardSprites.type.innerText = "";
 }
 
 async function drawButton(text) {
@@ -172,6 +193,9 @@ async function playAudio(status) {
 }
 
 function init() {
+    
+    showHiddenCardFieldsImages(false);
+
     drawCards(5, playerSides.player1);
     drawCards(5, playerSides.computer);
 }
